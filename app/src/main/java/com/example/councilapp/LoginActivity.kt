9d177f008d8948830679faca2eee4b388ef3670e
FunkyPigz <103180465+FunkyPigz@users.noreply.github.com>
2022-05-02 23:25:11 +1000

    Login Page - added

--- app/src/main/java/com/example/councilapp/LoginActivity.kt
+++ app/src/main/java/com/example/councilapp/LoginActivity.kt
@@ -2,8 +2,12 @@ package com.example.councilapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.councilapp.databinding.ActivityMainBinding
+import com.example.practice_project.databinding.ActivityMainBinding
+
 
 class LoginActivity : AppCompatActivity() {
+
 
     private lateinit var binding: ActivityMainBinding
 
@@ -17,3 +21,20 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 }
+
+/*
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//            setContentView(R.layout.activity_login)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.button.text = "login"
+    }
+}
+ */
