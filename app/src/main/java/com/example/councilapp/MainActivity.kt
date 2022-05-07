bbe2b04d42fe07ce3f5ccc993c9dd6c7ae6098cc
FunkyPigz <103180465+FunkyPigz@users.noreply.github.com>
2022-05-07 11:17:03 +1000

    Logout Page - code added (not complete)

--- app/src/main/java/com/example/councilapp/MainActivity.kt
+++ app/src/main/java/com/example/councilapp/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.councilapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+    private lateinit var user: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        user = FirebaseAuth.getInstance()
+
+        if (user.currentUser !=null) {
+            user.currenUser?.Let {
+
+                binding.tvUserEmail.text = it.email
+
+
+            }
+        }
+
+        binding.btnSignout.SetOnClickLister {
+            user.signOut()
+            startActivity(
+                Intent(
+                    packageContext:this,
+
+                )
+            )
+        }
+
     }
 }
+
