ad71609a16c24849ce9ca124b00d43917b9609d3
hoanglampham <4937910@student.swin.edu.au>
2022-05-07 14:26:22 +1000

    update-activity-for-creating-report-form

--- app/src/main/java/com/example/councilapp/ReportCreating.kt
+++ app/src/main/java/com/example/councilapp/ReportCreating.kt
@@ -3,101 +3,89 @@ package com.example.councilapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.text.TextUtils
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.auth.AuthResult
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import org.w3c.dom.Text
+import java.lang.Override as Override
 
 class ReportCreating : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        setContentView(R.layout.activity_report_creating)
 
-        val btn_register = findViewById<Button>(R.id.btn_register)
-        val registerEmail = findViewById<EditText>(R.id.registerEmail)
-        val password = findViewById<EditText>(R.id.registerPassword)
-        val confirmPassword = findViewById<EditText>(R.id.registerCPassword)
-        val username = findViewById<EditText>(R.id.registerUsername)
-        val btn_login = findViewById<Button>(R.id.loginBtn)
+        val reporterName = findViewById<EditText>(R.id.reporterName)
+        val reporterAddress = findViewById<EditText>(R.id.reporterAddress)
+        val reportLocation = findViewById<EditText>(R.id.reportLocation)
+        val assetType = findViewById<EditText>(R.id.type)
+        val btnImage = findViewById<Button>(R.id.btn_image)
+        val note =  findViewById<EditText>(R.id.notes)
+        val btnSubmit = findViewById<Button>(R.id.btn_submit)
+        val btnCancel = findViewById<Button>(R.id.btn_cancel)
 
-        btn_register.setOnClickListener {
+        btnSubmit.setOnClickListener {
             when {
-                TextUtils.isEmpty(registerEmail.text.toString().trim { it <= ' '}) -> {
+                TextUtils.isEmpty(reporterName.text.toString().trim { it <= ' '}) -> {
                     Toast.makeText(
                         this@ReportCreating,
-                        "Please Enter Email Address.",
-                        Toast.LENGTH_SHORT
+                        "Please enter your name here",
+                        Toast.LENGTH_LONG
                     ).show()
                 }
 
-                TextUtils.isEmpty(password.text.toString().trim { it <= ' '}) -> {
+                TextUtils.isEmpty(reporterAddress.text.toString().trim { it <= ' '}) -> {
                     Toast.makeText(
                         this@ReportCreating,
-                        "Please Enter Password.",
-                        Toast.LENGTH_SHORT
+                        "Please enter your address here",
+                        Toast.LENGTH_LONG
                     ).show()
                 }
 
-                TextUtils.isEmpty(confirmPassword.text.toString().trim { it <= ' '}) -> {
+                TextUtils.isEmpty(reportLocation.text.toString().trim { it <= ' '}) -> {
                     Toast.makeText(
                         this@ReportCreating,
-                        "Please Confirm Password.",
-                        Toast.LENGTH_SHORT
+                        "Please enter asset location",
+                        Toast.LENGTH_LONG
                     ).show()
                 }
 
-                TextUtils.isEmpty(username.text.toString().trim { it <= ' '}) -> {
+                TextUtils.isEmpty(assetType.text.toString().trim { it <= ' '}) -> {
                     Toast.makeText(
                         this@ReportCreating,
-                        "Please Enter a Username.",
-                        Toast.LENGTH_SHORT
+                        "Please enter type of asset here",
+                        Toast.LENGTH_LONG
                     ).show()
                 }
+
+                TextUtils.isEmpty(note.text.toString().trim { it <= ' '}) -> {
+                    Toast.makeText(
+                        this@ReportCreating,
+                        "Please enter your notes here",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+
 
             else -> {
-                val email: String = registerEmail.text.toString().trim { it <= ' '}
-                val _password: String = password.text.toString().trim { it <= ' '}
-                val _username: String = username.text.toString()
 
-                FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, _password).addOnCompleteListener { task ->
-                    if (task.isSuccessful) {
-                        val firebaseUser: FirebaseUser = task.result!!.user!!
-
-                        Toast.makeText(
-                            this@ReportCreating,
-                            "You Have Been Successfully Registered",
-                            Toast.LENGTH_SHORT
-                        ).show()
-
-                        val intent =
-                            Intent(this@ReportCreating, MainActivity::class.java )
-                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                        intent.putExtra("user_id", firebaseUser.uid)
-                        intent.putExtra("email_id", email)
-                        startActivity(intent)
-                        finish()
-                    } else {
-                        Toast.makeText(
-                            this@ReportCreating,
-                            task.exception!!.message.toString(),
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
+                Toast.makeText(
+                    this@ReportCreating,
+                    "You Have Been Successfully Registered",
+                    Toast.LENGTH_LONG
+                ).show()
                 }
-
             }
-            }
-
         }
 
-        btn_login.setOnClickListener{
+        btnImage.setOnClickListener {
+            //
+        }
 
-            startActivity(Intent(this@ReportCreating, LoginActivity::class.java))
+        btnCancel.setOnClickListener{
+            startActivity(Intent(this@ReportCreating, ReportCreating::class.java))
         }
     }
 }
+
