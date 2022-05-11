c9b68f60e68e2608910d2817f07b3c9e708006f8
PikeyAU <66731279+PikeyAU@users.noreply.github.com>
2022-05-11 16:42:14 +1000

    Revert "Logout Page - code added (not complete)"
    This reverts commit bbe2b04d42fe07ce3f5ccc993c9dd6c7ae6098cc.

--- app/src/main/java/com/example/councilapp/MainActivity.kt
+++ app/src/main/java/com/example/councilapp/MainActivity.kt
@@ -1,40 +1,11 @@
 package com.example.councilapp
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivitySecondBinding
-    private lateinit var user: FirebaseAuth
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySecondBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        user = FirebaseAuth.getInstance()
-
-        if (user.currentUser !=null) {
-            user.currenUser?.Let {
-
-                binding.tvUserEmail.text = it.email
-
-
-            }
-        }
-
-        binding.btnSignout.SetOnClickLister {
-            user.signOut()
-            startActivity(
-                Intent(
-                    packageContext:this,
-
-                )
-            )
-        }
-
+        setContentView(R.layout.activity_main)
     }
 }
-
